5fb96088edc48e864512ae1402fa48429af25906
sonimwangi <162610347+sonimwangi@users.noreply.github.com>
2024-10-16 01:48:18 +0300

    Create lawi.sol

---
+++ lawi.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract SimpleStorage{
+    bool public isTrue;
+    uint public uintNumber;
+    int public intNumber;
+    string public name;
+    address public MyAccount;
+
+}
